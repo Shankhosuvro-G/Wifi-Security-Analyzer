c2063e34c228f756e7fc80cb65366d8a2dfd227e
Shankhosuvro Ghosh <98182979+Shankhosuvro-G@users.noreply.github.com>
2024-06-30 08:54:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,17 +6,25 @@ def analyze_wifi_security():
     command_output = subprocess.run(["netsh", "wlan", "show", "interfaces"], capture_output=True, text=True).stdout
     security_type_search = re.search(r'Authentication\s+:\s+(\w+)', command_output)
     ssid_search = re.search(r'SSID\s+:\s+(.+)', command_output)
+    channel_search= re.search(r'Channel\s+:\s+(\d+)',command_output)
     if security_type_search and ssid_search:
         security_type = security_type_search.group(1)
         ssid=ssid_search.group(1)
+        channel=int(channel_search.group(1))
+        if 1<= channel <=13:
+            freq="2.4Ghz"
+        elif 36<= channel <=165:
+            freq="5Ghz"
+        else:
+            freq="Unknown Frquency"        
         print(f"Network detected with ssid {ssid} and is set to passive mode.")
         print("Determining Wireless Security Protocol......")
         if security_type in ["WPA2", "WPA3"]:
-            return f"The network with ssid {ssid} is secured with {security_type}."
+            return f"The network with ssid {ssid} and frequency of {freq} is secured with {security_type}."
         elif security_type == "WEP":
-            return f"The network with ssid {ssid} is secured with WEP (not recommended due to security vulnerabilities)."
+            return f"The network with ssid {ssid} and frequency of {freq} is secured with WEP (not recommended due to security vulnerabilities)."
         elif security_type == "Open":
-            return f"The network with ssid {ssid} has no security (Open network)."
+            return f"The network with ssid {ssid} and frequency of {freq} has no security (Open network)."
         else:
             return "The network security type is not recognized."
     else:
